cb4a274a7063e5f2bab71bf87a9d4c7b17a0a1f4
nithinsubbiah <nithinsubbiah@gmail.com>
2024-12-05 09:54:08 -0800

    Revert "[mlir][Func] Delete `DecomposeCallGraphTypes.cpp` (#117424)"
    This reverts commit 7267c85959aa2490e2950f7fb817a76af7e94043.
    
    Signed-off-by: nithinsubbiah <nithinsubbiah@gmail.com>

--- mlir/lib/Transforms/Utils/DialectConversion.cpp
+++ mlir/lib/Transforms/Utils/DialectConversion.cpp
@@ -2568,7 +2568,6 @@ legalizeUnresolvedMaterialization(RewriterBase &rewriter,
     if (newMaterialization) {
       assert(newMaterialization.getType() == outputType &&
              "materialization callback produced value of incorrect type");
-#endif // NDEBUG
       rewriter.replaceOp(op, newMaterialization);
       return success();
     }
