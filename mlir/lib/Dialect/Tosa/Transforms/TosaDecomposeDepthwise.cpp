a3cba7e9a7880d01beeae933d2c464c47754fc48
Benoit Jacob <jacob.benoit.1@gmail.com>
2025-02-07 21:16:43 -0600

    Revert "[mlir][tosa] Change 'shape' of RESHAPE from attribute to input shape … (#125789)"
    This reverts commit 571a98722f5cabfc702aa1d307ffee9ebbef6e48.

--- mlir/lib/Dialect/Tosa/Transforms/TosaDecomposeDepthwise.cpp
+++ mlir/lib/Dialect/Tosa/Transforms/TosaDecomposeDepthwise.cpp
@@ -55,11 +55,10 @@ struct DepthwiseConv2DIsMul : public OpRewritePattern<tosa::DepthwiseConv2DOp> {
     inputType = RankedTensorType::get(
         revisedInputShape,
         dyn_cast<RankedTensorType>(input.getType()).getElementType());
-    auto revisedInputShapeValue =
-        getTosaConstShape(rewriter, op.getLoc(), revisedInputShape);
     input = rewriter
-                .create<tosa::ReshapeOp>(op.getLoc(), inputType, input,
-                                         revisedInputShapeValue)
+                .create<tosa::ReshapeOp>(
+                    op.getLoc(), inputType, input,
+                    rewriter.getDenseI64ArrayAttr(revisedInputShape))
                 .getResult();
 
     Type inputETy = inputType.getElementType();
@@ -154,10 +153,9 @@ struct DepthwiseConv2DIsMul : public OpRewritePattern<tosa::DepthwiseConv2DOp> {
     auto outputShapeType = RankedTensorType::get(
         outputShape,
         dyn_cast<RankedTensorType>(input.getType()).getElementType());
-    auto outputShapeValue =
-        getTosaConstShape(rewriter, op->getLoc(), outputShape);
     Value outputValue = rewriter.create<tosa::ReshapeOp>(
-        op.getLoc(), outputShapeType, mulValue, outputShapeValue);
+        op.getLoc(), outputShapeType, mulValue,
+        rewriter.getDenseI64ArrayAttr(outputShape));
 
     Value bias = op.getBias();
     if (EqualizeRanks(rewriter, op.getLoc(), outputValue, bias).failed()) {
