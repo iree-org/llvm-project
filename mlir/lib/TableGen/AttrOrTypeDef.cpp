1abd08e3a518219beda8be0aae80cad94557351a
Stanley Winata <stanley.winata@amd.com>
2025-04-23 08:09:02 -0700

    Revert "[MLIR][TableGen] Error on APInt parameter without custom comparator (#135970)"
    This reverts commit 4bcc414af3782c333f3d535c0e0a92e6120868f5.

--- mlir/lib/TableGen/AttrOrTypeDef.cpp
+++ mlir/lib/TableGen/AttrOrTypeDef.cpp
@@ -282,10 +282,6 @@ std::optional<StringRef> AttrOrTypeParameter::getAllocator() const {
   return getDefValue<StringInit>("allocator");
 }
 
-bool AttrOrTypeParameter::hasCustomComparator() const {
-  return getDefValue<StringInit>("comparator").has_value();
-}
-
 StringRef AttrOrTypeParameter::getComparator() const {
   return getDefValue<StringInit>("comparator").value_or("$_lhs == $_rhs");
 }
