fff0f4c410947fed3674d8d9154ab6a8744584b7
hanhanW <hanhan0912@gmail.com>
2025-07-18 17:56:19 -0700

    [mlir][linalg] Remove extra padding constraint.
    Signed-off-by: hanhanW <hanhan0912@gmail.com>

--- mlir/lib/Dialect/Linalg/Transforms/TilingInterfaceImpl.cpp
+++ mlir/lib/Dialect/Linalg/Transforms/TilingInterfaceImpl.cpp
@@ -932,25 +932,6 @@ struct PackOpTiling
           continue;
         }
 
-        // If the dimension needs extra padding, it is not supported because
-        // there are iterations that only write padding values to the whole
-        // tile. The consumer fusion is driven by the source, so it is not
-        // possible to map an empty slice to the tile. Extra padding is not a
-        // regular form, and the implementation is being conversative.
-        bool needExtraPadding = true;
-        if (!ShapedType::isDynamic(srcDimSize) &&
-            !ShapedType::isDynamic(destDimSize) && cstInnerSize) {
-          needExtraPadding =
-              destDimSize >
-              (srcDimSize + cstInnerSize.value() - 1) / cstInnerSize.value();
-        }
-        // Prioritize the case that the op already says that it does not need
-        // padding.
-        if (!packOp.getPaddingValue())
-          needExtraPadding = false;
-        if (needExtraPadding)
-          return failure();
-
         // Currently fusing `packOp` as consumer only expects perfect tiling
         // scenario because even if without padding semantic, the `packOp` may
         // also yield incomplete tiles. E.g. tensor<30xf32> -> tensor<5x6xf32>,
