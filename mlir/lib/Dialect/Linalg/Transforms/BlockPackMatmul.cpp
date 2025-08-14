5d115e49ba703e73e52dde1d621a7be31d9cc369
Jakub Kuderski <jakub@nod-labs.com>
2025-08-14 11:02:13 -0400

    Revert "[MLIR][Linalg] Remove matmul_transpose variants (#147961)"
    This reverts commit d15280894bd03c618ec1b4debcbfdb1fecb81594.

--- mlir/lib/Dialect/Linalg/Transforms/BlockPackMatmul.cpp
+++ mlir/lib/Dialect/Linalg/Transforms/BlockPackMatmul.cpp
@@ -320,6 +320,10 @@ void linalg::populateBlockPackMatmulPatterns(
     RewritePatternSet &patterns, const ControlBlockPackMatmulFn &controlFn) {
   patterns.add<BlockPackMatmul<linalg::GenericOp>,
                BlockPackMatmul<linalg::MatmulOp>,
-               BlockPackMatmul<linalg::BatchMatmulOp>>(patterns.getContext(),
-                                                       controlFn);
+               BlockPackMatmul<linalg::BatchMatmulOp>,
+               BlockPackMatmul<linalg::MatmulTransposeAOp>,
+               BlockPackMatmul<linalg::BatchMatmulTransposeAOp>,
+               BlockPackMatmul<linalg::MatmulTransposeBOp>,
+               BlockPackMatmul<linalg::BatchMatmulTransposeBOp>>(
+      patterns.getContext(), controlFn);
 }
