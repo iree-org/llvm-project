82201f66369b744d4e9ba0ae1074851433a36f70
Rahul Kayaith <rkayaith@gmail.com>
2025-09-08 23:07:41 +0000

    Revert "[mlir][Transforms] Add support for `ConversionPatternRewriter::replaceAllUsesWith` (#155244)"
    This reverts commit 2929a2978cc3bdb0ff12a0e5d0a9236ff221f668.

--- mlir/lib/Conversion/FuncToLLVM/FuncToLLVM.cpp
+++ mlir/lib/Conversion/FuncToLLVM/FuncToLLVM.cpp
@@ -284,7 +284,7 @@ static void restoreByValRefArgumentType(
         cast<TypeAttr>(byValRefAttr->getValue()).getValue());
 
     Value valueArg = LLVM::LoadOp::create(rewriter, arg.getLoc(), resTy, arg);
-    rewriter.replaceAllUsesWith(arg, valueArg);
+    rewriter.replaceUsesOfBlockArgument(arg, valueArg);
   }
 }
 
