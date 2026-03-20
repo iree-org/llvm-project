704f13b31c621e23077c04d17e238c340550dcb2
Eric Feng <Eric.Feng@amd.com>
2026-03-20 14:58:19 -0700

    Revert "[AMDGPU] DPP implementations for Wave Reduction (#185814)"
    This reverts commit fbd24677963abedf88b6dbed277764e955374dd8.

--- llvm/lib/Target/AMDGPU/SIInstrInfo.h
+++ llvm/lib/Target/AMDGPU/SIInstrInfo.h
@@ -1361,7 +1361,6 @@ public:
                          StringRef &ErrInfo) const override;
 
   unsigned getVALUOp(const MachineInstr &MI) const;
-  unsigned getVALUOp(unsigned Opc) const;
 
   void insertScratchExecCopy(MachineFunction &MF, MachineBasicBlock &MBB,
                              MachineBasicBlock::iterator MBBI,
